44c8dfda342830dd2a2afae302c3758d2c297e7c
clowns123 <clowns1232@gmail.com>
2020-06-28 23:00:16 +0900

    js 정리

--- projectTest/tmdb.js
+++
@@ -1,9 +0,0 @@
-function getList(apiKey){
-  const LANGUAGE = '&language=ko-kr';
-  const BASEURL = 'http://api.themoviedb.org/3';  
-  const IMAGEURL = 'http://image.tmdb.org/t/p';
-
-  function getTv(){
-
-  }
-}
